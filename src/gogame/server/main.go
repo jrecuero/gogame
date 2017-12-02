77d9577ab10c5e1535011df5811b96abe2031468
Jose Carlos Recuero Arias <jose.recuero@gmail.com>
2017-12-01 16:21:18 -0800

    Update server documentation.

--- src/gogame/server/main.go
+++ src/gogame/server/main.go
@@ -8,6 +8,7 @@ func ping(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("pomg"))
 }
 
+// Server function provides server basic functionality.
 func Server() {
 	http.HandleFunc("/ping", ping)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
